9444470e535390dcb880a11f6bb5bbe036b40f32
upadhyayabhisek <g38099933@gmail.com>
2024-10-15 17:00:25 +0545

    Initial upload
    project regarding construction freelancing

---
+++ registerPage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="">
+        <input type="button" value="ok">
+    </form>
+
+    <?php
+    include 'include/footer.php';
+    ?>
+
+</body>
+
+</html>
