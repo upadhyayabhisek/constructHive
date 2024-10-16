03ac7fc6d78c4e3e9f943fd5388d7e1f6740c0c0
upadhyayabhisek <g38099933@gmail.com>
2024-10-16 17:59:31 +0545

    Add files via upload

--- registerPage.php
+++ registerPage.php
@@ -9,6 +9,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register an Account</title>
+    <link rel="stylesheet" href="css/styles.css">
     <script src="js/website_JavaScript.js" defer></script>
 </head>
 
